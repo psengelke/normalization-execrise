4d296c95cdc1494c5f9733ea5804b6fdddebd12a
izakblom <u13126777@tuks.co.za>
2015-10-03 10:12:58 +0200

    Browse for file

--- src/ParserController.java
+++ src/ParserController.java
@@ -1,12 +1,19 @@
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
 import java.util.Random;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileFilter;
 
 /**
  * 
@@ -90,16 +97,22 @@ public class ParserController {
 		return report;
 	}
 	
-	public static void main(){
+	public static void main(String[] args){
 		
 		JFrame frame = new JFrame();
 		ParserController pc = new ParserController();
 		MyActionListener listener = pc.new MyActionListener();
-		JButton parseButton = new JButton("parse");
-		parseButton.setBounds(100,150, 80, 30);
+		JButton parseButton = new JButton("Parse File");
+		parseButton.setBounds(120,150, 100, 40);
 		parseButton.addActionListener(listener);
 		
-		frame.setSize(400, 500);
+		JFilePicker fp = pc.new JFilePicker("Choose a file to parse: ", "Browse");
+		fp.setBounds(100, 50, 600, 100);
+		fp.setMode(1);
+		
+		frame.add(fp);
+		frame.add(parseButton);
+		frame.setSize(800, 600);
 		frame.setLayout(null);
 		frame.setVisible(true);
 	}
@@ -112,5 +125,101 @@ public class ParserController {
 			// TODO Auto-generated method stub
 			
 		}
+	}
+	
+	public class JFilePicker extends JPanel {
+	    private String textFieldLabel;
+	    private String buttonLabel;
+	     
+	    private JLabel label;
+	    private JTextField textField;
+	    private JButton button;
+	     
+	    private JFileChooser fileChooser;
+	     
+	    private int mode;
+	    public static final int MODE_OPEN = 1;
+	    public static final int MODE_SAVE = 2;
+	     
+	    public JFilePicker(String textFieldLabel, String buttonLabel) {
+	        this.textFieldLabel = textFieldLabel;
+	        this.buttonLabel = buttonLabel;
+	         
+	        fileChooser = new JFileChooser();
+	         
+	        setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+	 
+	        // creates the GUI
+	        label = new JLabel(textFieldLabel);
+	         
+	        textField = new JTextField(30);
+	        button = new JButton(buttonLabel);
+	         
+	        button.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent evt) {
+	                buttonActionPerformed(evt);            
+	            }
+	        });
+	         
+	        add(label);
+	        add(textField);
+	        add(button);
+	         
+	    }
+	     
+	    private void buttonActionPerformed(ActionEvent evt) {
+	    	if (mode == MODE_OPEN) {
+	            if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+	                textField.setText(fileChooser.getSelectedFile().getAbsolutePath());
+	            }
+	        } else if (mode == MODE_SAVE) {
+	            if (fileChooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+	                textField.setText(fileChooser.getSelectedFile().getAbsolutePath());
+	            }
+	        }
+	    }
+	 
+	    public void addFileTypeFilter(String extension, String description) {
+	        FileTypeFilter filter = new FileTypeFilter(extension, description);
+	        fileChooser.addChoosableFileFilter(filter);
+	    }
+	     
+	    public void setMode(int mode) {
+	        this.mode = mode;
+	    }
+	     
+	    public String getSelectedFilePath() {
+	        return textField.getText();
+	    }
+	     
+	    public JFileChooser getFileChooser() {
+	        return this.fileChooser;
+	    }
+	}
+	
+	public class FileTypeFilter extends FileFilter {
+		 
+	    private String extension;
+	    private String description;
+	     
+	    public FileTypeFilter(String extension, String description) {
+	        this.extension = extension;
+	        this.description = description;
+	    }
+	     
+	    @Override
+	    public boolean accept(File file) {
+	        if (file.isDirectory()) {
+	            return true;
+	        }
+	        return file.getName().toLowerCase().endsWith(extension);
+	    }
+	     
+	    public String getDescription() {
+	        return description + String.format(" (*%s)", extension);
+	    }
+
+		
 	}
 }
