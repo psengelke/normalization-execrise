bf44538126e8f0a1d4c9632a64d561d00987fdab
izakblom <u13126777@tuks.co.za>
2015-10-03 09:04:51 +0200

    countCodeLines

--- src/SourceFileParser.java
+++ src/SourceFileParser.java
@@ -10,8 +10,26 @@ public class SourceFileParser {
 	private SourceFileParser(){}
 
 	public static int countCodeLines(LinkedList<String> file){
-		
-		return 0;
+		int count = 0;
+		boolean commentOn = false;
+		for (int k = 0; k < file.size(); ++k)
+		{
+			if (file.get(k).contains("//") || commentOn)
+				continue;
+			else if (file.get(k).contains("/*"))
+			{
+				commentOn = true;
+				continue;
+			}
+			else if (file.get(k).contains("*/"))
+			{
+				commentOn = false;
+				continue;
+			}
+			else
+				count++;
+		}
+		return count;
 	}
 	
 	public static int countStatements(LinkedList<String> file){
